96a300d2f27132009b08998ef46c5a34403c3b19
renehernandez <renehr9102@gmail.com>
2018-09-16 07:57:39 -0400

    Fix social share buttons
    Change common tags to common topics
    Remove unused css code

--- assets/scripts/social_sharing.js
+++ assets/scripts/social_sharing.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function($) {
+    $sharingData = {}; 
+    $sharingData.title = $('.sharingData-title').text();
+    $sharingData.url = $('.sharingData-url').text();
+
     $('.rrssb-buttons').rrssb({
         // required:
-        title: '{{post.title}}',
-        url: '{{@blog.url}}{{post.url}}',
+        title: $sharingData.title,
+        url: $sharingData.url,
 
         // optional:
-         description: '{{post.title}}',
+         description: $sharingData.title,
         // emailBody: 'Usually email body is just the description + url, but you can customize it if you want'
     });
 });
