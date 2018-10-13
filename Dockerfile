68deaf9a156358dae830b682b05113ba50126f8c
renehernandez <renehr9102@gmail.com>
2018-10-13 00:33:35 -0400

    Add build step to generate dist with optimized assets
    Add dockerfile to include theme as part of image definition
    Update package.json with uglify and filter plugins

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ghost:2.2.2
+
+COPY ./ /var/lib/ghost/content/themes/bitknown
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -20,12 +22,42 @@ var swallowError = function swallowError(error) {
     this.emit('end');
 };
 
-var nodemonServerInit = function () {
-    livereload.listen(1234);
-};
+gulp.task('build', ['css', 'js'], function (/* cb */) {
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
 
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
+    return gulp.src([
+        '**',
+        '!node_modules', 
+        '!node_modules/**',
+        '!dist', 
+        '!dist/**',
+        '!Dockerfile',
+        '!.gitignore',
+        '!gulpfile.js',
+        '!yarn.lock',
+        '!travis.yml',
+        '!assets/scripts',
+        '!assets/scripts/**',
+        '!assets/styles',
+        '!assets/styles/**'
+    ])
+        .pipe(gulp.dest(targetDir));
+});
+
+gulp.task('js', function () {
+    var jsFilter = filter(['**/*.js'], {restore: true});
+
+    return gulp.src('assets/scripts/**/*.js')
+        .on('error', swallowError)
+        .pipe(sourcemaps.init())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/assets/scripts'))
+        .pipe(livereload());
 });
 
 gulp.task('css', function () {
@@ -37,33 +69,15 @@ gulp.task('css', function () {
         cssnano()
     ];
 
-    return gulp.src('assets/styles/*.css')
+    return gulp.src('assets/styles/**/*.css')
         .on('error', swallowError)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
+        .pipe(gulp.dest('dist/assets/styles'))
         .pipe(livereload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch('assets/styles/**', ['css']);
-});
-
-gulp.task('zip', ['css'], function () {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
-
-    return gulp.src([
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!dist', '!dist/**'
-    ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
-});
 
 gulp.task('default', ['build'], function () {
-    gulp.start('watch');
 });
